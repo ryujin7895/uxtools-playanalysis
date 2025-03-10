9caff59a2a82411775b480cbcf27714c92e14ffc
Ryujin <w230037@e.ntu.edu.sg>
2025-03-10 12:37:32 +0700

    Edit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from '@vercel/remix/vite';
 
 declare module "@remix-run/node" {
   interface Future {
@@ -12,7 +11,6 @@ declare module "@remix-run/node" {
 export default defineConfig({
   plugins: [
     remix({
-      presets: [vercelPreset()],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
